78b559609eaa6eab2d71b31c8d8f74a0c87a9c15
Fahril Hadi <fahrilhadi77@gmail.com>
2025-05-01 19:12:09 +0700

    entry

---
+++ entry_test.go
@@ -0,0 +1,16 @@
+package golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestEntry(t *testing.T)  {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	entry := logrus.NewEntry(logger)
+	entry.WithField("username", "fahrilhadi")
+	entry.Info("Hello Entry")
+}
